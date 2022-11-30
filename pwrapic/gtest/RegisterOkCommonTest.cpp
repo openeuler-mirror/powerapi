02e0a047dfadd9f65d064b79eb656dbe06b48666
jimmy-jiang-junior <jiangyi38@hisilicon.com>
2022-11-30 16:18:45 +0800

    add new testcase
    add testcase for PWR_SYS_SetPowerState
    add duplication check in PWR_CPU_GetUsage_Test_001
    add flexible time check in PWR_SetMetaDataCallback_Test_002
    modify testcase PWR_CPU_GetFreq_Test_002 for spec = 1
    call PWR_CPU_SetFreqRange only when cpufreq driver is cppc_cpufreq
        in PWR_CPU_SetFreqRange_Test_001

--- pwrapic/gtest/RegisterOkCommonTest.cpp
+++ pwrapic/gtest/RegisterOkCommonTest.cpp
@@ -12,6 +12,8 @@
  * Create: 2022-11-04
  * Description: PowerAPI testcase. All the cases in This file are based on testsuit RegisterOkTestCommon
  * **************************************************************************** */
+#include <ctime>
+#include <string>
 #include <gtest/gtest.h>
 #include "GtestLog.h"
 #include "powerapi.h"
@@ -57,7 +59,7 @@ TEST_F(RegisterOkTestCommon, PWR_CreateDcTask_Test_001)
 /*
  * 功能描述: 注册两个回调任务, 校验注册函数和删除函数工作正常
  */
-TEST_F(RegisterOkTestCommon, PWR_SetMetaDataCallback_Test_001)
+TEST_F(RegisterOkTestCommon, PWR_DeleteDcTask_Test_001)
 {
     int ret = SUCCESS;
     EXPECT_EQ(SUCCESS, PWR_SetMetaDataCallback(MetaDataCallback));
@@ -81,7 +83,7 @@ TEST_F(RegisterOkTestCommon, PWR_SetMetaDataCallback_Test_001)
 /*
  * 功能描述: PWR_SetMetaDataCallbak任务入参为NULL, 返回失败
  */
-TEST_F(RegisterOkTestCommon, PWR_SetMetaDataCallback_Test_002)
+TEST_F(RegisterOkTestCommon, PWR_SetMetaDataCallback_Test_001)
 {
     EXPECT_NE(SUCCESS, PWR_SetMetaDataCallback(NULL));
 }
@@ -90,7 +92,7 @@ TEST_F(RegisterOkTestCommon, PWR_SetMetaDataCallback_Test_002)
  * 功能描述: 通过lamda函数创建回调函数, 注册回调任务获取ipc和llcMiss
  * 在回调函数中对ipc和llcMiss做大于0的校验
  */
-TEST_F(RegisterOkTestCommon, PWR_SetMetaDataCallback_Test_003)
+TEST_F(RegisterOkTestCommon, PWR_SetMetaDataCallback_Test_002)
 {
     EXPECT_EQ(SUCCESS,
         PWR_SetMetaDataCallback([](const PWR_COM_CallbackData *callbackData) {
@@ -98,8 +100,19 @@ TEST_F(RegisterOkTestCommon, PWR_SetMetaDataCallback_Test_003)
             PWR_CPU_PerfData *perfData = (PWR_CPU_PerfData *)(callbackData->data);
             EXPECT_LT(0, perfData->ipc);
             EXPECT_LT(0, perfData->llcMiss);
+
+            time_t curTime;
+            tm *nowTime;
+            time(&curTime); // 获取1970到目前经过秒数
+            nowTime = localtime(&curTime); // 输出tm结构的年月日
+
+            int year = 1900 + nowTime->tm_year;
+            int month = 1 + nowTime->tm_mon;
+            int day = nowTime->tm_mday;
             std::string str = callbackData->ctime;
-            EXPECT_NE(std::string::npos, str.find("2022"));
+            EXPECT_NE(std::string::npos, str.find(std::to_string(year)));
+            EXPECT_NE(std::string::npos, str.find(std::to_string(month)));
+            EXPECT_NE(std::string::npos, str.find(std::to_string(day)));
             printf("[TASK]Get PERF data. ipc: %f  miss: %f, ctime:%s\n", perfData->ipc, perfData->llcMiss,
                 callbackData->ctime);
         }));
@@ -113,23 +126,9 @@ TEST_F(RegisterOkTestCommon, PWR_SetMetaDataCallback_Test_003)
 }
 
 /*
- * 功能描述: PWR_RequestControlAuth()接口校验
- * 获取权限后调用Set类接口成功, 释放接口后调用Set类接口失败
- */
-TEST_F(RegisterOkTestCommon, PWR_RequestControlAuth_Test_001)
-{
-    char governor[] = "performance";
-    EXPECT_EQ(SUCCESS, PWR_RequestControlAuth());
-    EXPECT_EQ(SUCCESS, PWR_RequestControlAuth());
-    EXPECT_EQ(SUCCESS, PWR_CPU_SetFreqGovernor(governor));
-    EXPECT_EQ(SUCCESS, PWR_ReleaseControlAuth());
-    EXPECT_NE(SUCCESS, PWR_CPU_SetFreqGovernor(governor));
-}
-
-/*
  * 功能描述: 由于没有权限, 所有的SET操作都会失败, 所有的Get操作都可以成功
  */
-TEST_F(RegisterOkTestCommon, PWR_RequestControlAuth_Test_002)
+TEST_F(RegisterOkTestCommon, PWR_RequestControlAuth_Test_001)
 {
     PWR_CPU_Info cpuInfo;
     EXPECT_EQ(SUCCESS, PWR_CPU_GetInfo(&cpuInfo));
@@ -154,5 +153,19 @@ TEST_F(RegisterOkTestCommon, PWR_RequestControlAuth_Test_002)
     EXPECT_NE(SUCCESS, PWR_SYS_SetPowerState(1));
 
     char governor[] = "performance";
+    EXPECT_NE(SUCCESS, PWR_CPU_SetFreqGovernor(governor));
+}
+
+/*
+ * 功能描述: PWR_RequestControlAuth()接口校验
+ * 获取权限后调用Set类接口成功, 释放接口后调用Set类接口失败
+ */
+TEST_F(RegisterOkTestCommon, PWR_ReleaseControlAuth_Test_001)
+{
+    char governor[] = "performance";
+    EXPECT_EQ(SUCCESS, PWR_RequestControlAuth());
+    EXPECT_EQ(SUCCESS, PWR_RequestControlAuth());
+    EXPECT_EQ(SUCCESS, PWR_CPU_SetFreqGovernor(governor));
+    EXPECT_EQ(SUCCESS, PWR_ReleaseControlAuth());
     EXPECT_NE(SUCCESS, PWR_CPU_SetFreqGovernor(governor));
 }
